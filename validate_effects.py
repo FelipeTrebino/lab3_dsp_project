2a1363d528755b10a484beb754791978ea3fc66c
Felipe Trebino <fliphd1505@gmail.com>
2025-12-06 08:29:35 -0300

    feat: pitch shift

--- validate_effects.py
+++ validate_effects.py
@@ -3,6 +3,7 @@ from file_manager import AudioManager
 from effects.reverb import apply_reverb
 from effects.flanger import apply_flanger
 from effects.tremolo import apply_tremolo
+from pitch_shift.shift_assets import shift_to_note
 
 from audio_io import save_wav
 
@@ -39,8 +40,13 @@ def main():
         audio_original,
         fs
     )
-      
-    save_wav("output/reverb_test.wav", fs, processed_audio_reverb)
+    
+    reverb_A4 = shift_to_note(processed_audio_reverb, fs, target_note="A4", root_note="C4")
+    reverb_A3 = shift_to_note(processed_audio_reverb, fs, target_note="A3", root_note="C4")
+    
+          
+    save_wav("output/reverb_test_A4.wav", fs, reverb_A4)
+    save_wav("output/reverb_test_A3.wav", fs, reverb_A3)
     save_wav("output/flanger_test.wav", fs, processed_audio_flanger)
     save_wav("output/tremolo_test.wav", fs, processed_audio_tremolo)
 
